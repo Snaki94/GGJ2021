f0ae012d54c98165823736dc54163efc2ea60e22
Sebastian Adamowski <s.adamowski94@gmail.com>
2021-01-30 14:53:10 +0100

    Finished grid layout

--- GGJ2021/Assets/Scripts/FlexibleGridLayout.cs
+++ GGJ2021/Assets/Scripts/FlexibleGridLayout.cs
@@ -1,22 +1,86 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
- 
-public class FlexibleGridLayout : MonoBehaviour
+
+public class FlexibleGridLayout : LayoutGroup
 {
-    float defaultRatio16_9 = 1.777133f;
-    GridLayoutGroup layout;
-    // Start is called before the first frame update
-    void Start()
+    public enum FitType
     {
-        layout = GetComponent<GridLayoutGroup>();
-        if (Camera.main is null) return;
-        var aspect = Camera.main.aspect;
-        var padding = layout.padding;
-        layout.cellSize /= aspect / defaultRatio16_9;
-        padding.left = (int) (padding.left / (aspect / defaultRatio16_9));
-        padding.right = (int) (padding.left / (aspect / defaultRatio16_9));
-        padding.top = (int) (padding.left / (aspect / defaultRatio16_9));
-        padding.bottom = (int) (padding.left / (aspect / defaultRatio16_9));
-        layout.spacing /= aspect / defaultRatio16_9;
+        Uniform,
+        Width,
+        Height,
+        FixedRows,
+        FixedColumns
+    }
+
+    public FitType fitType;
+
+    public int rows;
+    public int columns;
+    public Vector2 cellSize;
+    public Vector2 spacing;
+
+    public bool fitX;
+    public bool fitY;
+
+    public override void CalculateLayoutInputVertical()
+    {
+        base.CalculateLayoutInputHorizontal();
+
+        if(fitType == FitType.Width || fitType == FitType.Height || fitType == FitType.Uniform)
+        {
+            fitX = true;
+            fitY = true;
+            float sqrRt = Mathf.Sqrt(transform.childCount);
+            rows = Mathf.CeilToInt(sqrRt);
+            columns = Mathf.CeilToInt(sqrRt);
+        }
+
+        if(fitType == FitType.Width || fitType == FitType.FixedColumns || fitType == FitType.Uniform)
+        {
+            rows = Mathf.CeilToInt(transform.childCount / (float)columns);
+        }
+        if (fitType == FitType.Height || fitType == FitType.FixedRows || fitType == FitType.Uniform)
+        {
+            columns = Mathf.CeilToInt(transform.childCount / (float)rows);
+        }
+
+        float parentWidth = rectTransform.rect.width;
+        float parentHeight = rectTransform.rect.height;
+
+        float cellWidth = (parentWidth / (float)columns) - ((spacing.x / (float)columns) * (columns - 1)) - (padding.left / (float)columns) - (padding.right / (float)columns);
+        float cellHeight = (parentHeight / (float)rows) - ((spacing.y / (float)rows) * (rows - 1)) - (padding.top / (float)rows) - (padding.bottom / (float)rows);
+
+        cellSize.x = fitX ? cellWidth : cellSize.x;
+        cellSize.y = fitY ? cellHeight : cellSize.y;
+
+        int columnCount = 0;
+        int rowCount = 0;
+
+        for(int i = 0; i < rectChildren.Count; i ++)
+        {
+            rowCount = i / columns;
+            columnCount = i % columns;
+
+            var item = rectChildren[i];
+
+            var xPos = (cellSize.x * columnCount) + (spacing.x * columnCount) + padding.left;
+            var yPos = (cellSize.y * rowCount) + (spacing.y * rowCount) + padding.top;
+
+            SetChildAlongAxis(item, 0, xPos, cellSize.x);
+            SetChildAlongAxis(item, 1, yPos, cellSize.y);
+        }
+
+    }
+
+    public override void SetLayoutHorizontal()
+    {
+
+    }
+
+    public override void SetLayoutVertical()
+    {
+
     }
 }
